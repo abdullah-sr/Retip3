d6db740f615578a1f578f6c9e22b81d6144ab561
Abdullah Alradadi <abdullah@Abdullahs-Mac.local>
2016-07-11 09:45:02 -0700

    commit

---
+++ Retip/ViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, Dimmable {
+
+    let dimLevel: CGFloat = 0.5
+    let dimSpeed: Double = 0.5
+    
+    @IBOutlet weak var collectioView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CollectionViewCell
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(view.frame.width, view.frame.width + view.frame.width/4)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        dim(.In, alpha: dimLevel, speed: dimSpeed)
+    }
+    
+    @IBAction func unwindFromSecondary(segue: UIStoryboardSegue) {
+        dim(.Out, speed: dimSpeed)
+    }
+}
+
